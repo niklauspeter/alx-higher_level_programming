e6c6fdbbc78d8fbb4c7c117f9aa81da0172fb146
niklauspeter <oriokiklaus@gmail.com>
2022-12-01 10:22:01 -0800

    add for loop using ascii to print alphabets in lower case

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-import string
-
-for letter in string.ascii_lowercase:
-    print(letter, end = "")
+for character in range(97, 123):
+    print("{:c}".format(character), end='')
