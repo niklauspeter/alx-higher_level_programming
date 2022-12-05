e62a1162532f812a749918195b6b7994a0c6b226
niklauspeter <oriokiklaus@gmail.com>
2022-12-05 14:21:44 -0800

    make executable

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
    """prints list in reverse"""
-   last_index = (len(my_list) - 1)
-   for i in my_list:
-       if last_index >= 0:
-           print ("{:d}".format(my_list[last_index]))
-           last_index = last_index - 1
+   if isinstance(my_list, list):
+       last_index = (len(my_list) - 1)
+       for i in my_list:
+           if last_index >= 0:
+               print ("{:d}".format(my_list[last_index]))
+               last_index = last_index - 1
