34bd22b754e29eaa068ed118fddf4c1ffbfa1bb7
niklauspeter <oriokiklaus@gmail.com>
2022-12-05 14:08:24 -0800

    function prints elements of list in reverse

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    last_index = (len(my_list) - 1)
+    for i in my_list:
+        if last_index >= 0:
+            print ("{:d}".format(my_list[last_index]))
+            last_index = last_index - 1
