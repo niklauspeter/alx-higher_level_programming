0a976b5f3fd03e0353971b60fcf6f75506ce739e
niklauspeter <oriokiklaus@gmail.com>
2022-12-05 14:12:59 -0800

    checkif list is list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    last_index = (len(my_list) - 1)
-    for i in my_list:
-        if last_index >= 0:
-            print ("{:d}".format(my_list[last_index]))
-            last_index = last_index - 1
+   """prints list in reverse"""
+   if isinstance(my_list, list):
+       last_index = (len(my_list) - 1)
+       for i in my_list:
+           if last_index >= 0:
+               print ("{:d}".format(my_list[last_index]))
+               last_index = last_index - 1
