171d010a363ecca562c148700592c67aeb1e0aa5
niklauspeter <oriokiklaus@gmail.com>
2022-12-05 14:16:12 -0800

    check whereerror falls

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
    """prints list in reverse"""
-   if isinstance(my_list, list):
+   """if isinstance(my_list, list):"""
        last_index = (len(my_list) - 1)
        for i in my_list:
            if last_index >= 0:
