c846f3496815a45b1e1acd332a41c8da4aee2a33
niklauspeter <oriokiklaus@gmail.com>
2022-12-06 13:41:28 -0800

    change comparison to element within list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,6 +5,6 @@ def max_integer(my_list=[]):
 
     big = my_list[0]
     for i in range(len(my_list)):
-        if i > big:
+        if my_list[i] > big:
             big = my_list[i]
     return (big)
