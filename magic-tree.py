4590e7c965952d783fcf14aa7a50005a0001464b
niklauspeter <oriokiklaus@gmail.com>
2022-12-07 09:52:09 -0800

    set timer between prints to make it dramatic

--- magic-tree.py
+++ magic-tree.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+from time import sleep
 def magic_tree():
     a = 1
     b = 0
@@ -7,18 +8,19 @@ def magic_tree():
     spaces = [" "]
     spaces_num = 9
 
-    for i in range(10):
+    for i in range(9):
         b += 1
         list1.append(a)
        # print(list1)
         list2 = [str(i) for i in list1]
         list3 = "".join(list2)
-        spaces2 = [str(i) for i in spaces]
-        spaces3 = "".join(spaces2)
+        #spaces2 = [str(i) for i in spaces]
+        spaces3 = "".join(spaces)
        # print(list3)
         print("{}{} * 8 + {} = {}".format(spaces3*spaces_num, list3,c, int(list3)*8+c))
         a += 1
         c += 1
         spaces_num -= 1
+        sleep(1)
 
 magic_tree()
