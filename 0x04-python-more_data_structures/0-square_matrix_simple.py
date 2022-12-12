a6c2ddbd19448e3b6b5bd1048881391a74f57666
niklauspeter <oriokiklaus@gmail.com>
2022-12-12 12:14:36 -0800

    change return statement

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,5 +3,4 @@ def square_matrix_simple(matrix = []):
     """computethe square of all integers in a matrix"""
     if not matrix:
         return None
-    matrix2 = list(list(map(lambda x: x * x, num_list)) for num_list in matrix)
-    return matrix2
+    return list(list(map(lambda x: x * x, num_list)) for num_list in matrix)
