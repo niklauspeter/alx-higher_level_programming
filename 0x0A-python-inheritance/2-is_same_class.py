7426cd551ec5c046cf6666904649d69d636e3f74
niklauspeter <oriokiklaus@gmail.com>
2023-01-09 16:37:15 -0800

    remove trailing white space

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,7 +6,7 @@ def is_same_class(obj, a_class):
     function: is_same_class
     obj: an object
     a_class: a class
-    returns: Bool 
+    returns: Bool
     """
 
     if type(obj) == a_class:
