2269a340e27c685734181863ef4435ae222e63ae
klaus <oriokiklaus@gmail.com>
2023-01-14 02:45:26 +0300

    printsnumbers one to zero fizz for multiples of threee buzz for five and fizzbuzzforthree and five

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+for i in range(100):
+    if i % 3 == 0:
+        print("Fizz")
+    elif:
+        if i % 3 and i % 5:
+            print("FizzBuzz")
+    else:
+        print("i", end= "")
