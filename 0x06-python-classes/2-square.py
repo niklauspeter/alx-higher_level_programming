1b125f74993b74f9e24d30301b7b029ec7eedcb4
klaus <oriokiklaus@gmail.com>
2023-01-18 13:41:14 +0300

    class defines a square whoch must be an integer andn greater than o

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""class defines a square"""
+
+
+class Square:
+    """create private attribute size.
+    
+    args:
+    size must be int - else raise type error
+    
+    size must be less than 0 - elese raise exception error"""
+
+    def __init__(self, size=0):
+        """initialize a new square"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
