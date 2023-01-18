cbb918ac665c93a2d056700690d6f42405549746
klaus <oriokiklaus@gmail.com>
2023-01-18 21:25:38 +0300

    function takes in size of square and tuple as input, prints out squares in hashtag spaced out depending on the two dimensions described on tuple

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -21,10 +21,16 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
+    @property
     def position(self):
-        if not isinstance(self.__position, tuple):
+        """get the current position of the square"""
+        return (self.__position)
+        
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or len(value) !=2 or not all(isinstance(num, int) for num in value) or not all(num >=0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
         """return current area f square"""
@@ -34,11 +40,19 @@ class Square:
         """print out a square to stdin using # character"""
         if self.size == 0:
             print("")
-        for n in range(0, self.__size):
-            if self.position[1] > 0:
-                [print(((self.position[1])*'_') +"#", end="") for j in range(self.__size)]
-                print("")
+        
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
+    # return[print("") for i in range(0, self.__position[1])]
+    #     for n in range(0, self.__size):
+    #         if self.position[1] > 0:
+    #             [print(((self.position[1])*'_') +"#", end="") for j in range(self.__size)]
+    #             print("")
 
+        
         # for i in range(0, self.__size):
         #     for n in range(self.__size):
         #         [print("#", end="")]
