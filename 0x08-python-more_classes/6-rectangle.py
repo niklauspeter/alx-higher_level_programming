c893a997412190d81c45426985f01c2b8e9726e6
klaus <oriokiklaus@gmail.com>
2023-02-02 14:21:38 +0300

    change typo

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -12,7 +12,7 @@ class Rectangle:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
         """
-        type(self).number_of_isntances += 1
+        type(self).number_of_instances += 1
         self.width = width
         self.height = height
 
