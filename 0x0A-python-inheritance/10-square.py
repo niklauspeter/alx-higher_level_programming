2251c38acd7e258a29459126611db5bf5a17323a
klaus <oriokiklaus@gmail.com>
2023-02-08 21:55:39 +0300

    instantiates sides of a square

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-"""class square inherits from rectangle"""
 
-Rectangle = __import__("9-rectangle.py").Rectangle
+"""Defines a Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
+
 
 class Square(Rectangle):
-    """sintantiates size of sides of square"""
+    """Represent a square."""
 
     def __init__(self, size):
-        """reps a square"""
-
-
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-
-    #def area(self):
-    #    return self.__size * self.__size
-    
-    #def __str__(self):
-     #   """Return the print() and str() representation of a Rectangle."""
-      #  string = "[" + str(self.__class__.__name__) + "] "
-       # string += str(self.__size) + "/" + str(self.__size)
-        #return string
