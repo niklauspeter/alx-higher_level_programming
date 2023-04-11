94ec8498634262672fcb16e2b262a0160282d18c
klaus <oriokiklaus@gmail.com>
2023-04-12 01:54:37 +0300

    observe standards

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
-function add(a, b)
-{
-    return a + b;
+function add (a, b) {
+  return a + b;
 }
 const a = Number(process.argv[2]);
 const b = Number(process.argv[3]);
 
 if (a || b) {
-    console.log(add(a, b));
-}
-else {
-    console.log("NaN")
+  console.log(add(a, b));
+} else {
+  console.log('NaN');
 }
