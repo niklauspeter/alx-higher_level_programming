aadb38d4cf3deda988a7b9f3400153e93fab12d9
klaus <oriokiklaus@gmail.com>
2023-04-12 02:25:12 +0300

    functinon returns factorial recursively

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,17 +1,36 @@
 #!/usr/bin/node
-function factorial (a) {
-  let total = 1;
-  for (let i = 1; i < a; i++) {
-    total *= a;
-    a -= 1;
+function factorial(a) {
+    if (isNaN(a)) {
+      return 1;
+    } else if (a <= 1) {
+      return 1;
+    } else {
+      return a * factorial(a - 1);
+    }
   }
-  return total;
-}
+  
+  const num = parseInt(process.argv[2]);
+  
+  console.log(factorial(num));
 
-const a = Number(process.argv[2]);
 
-if (isNaN(a)) {
-  console.log(1);
-} else {
-  console.log(factorial(a));
-}
+// function factorial (a) {
+//   let total = 1;
+//   for (let i = 1; i < a; i++) {
+//     total *= a;
+//     a -= 1;
+//   }
+//   return total;
+// return a * factorial(a - 1);
+// }
+
+// const n = Number(process.argv[2]);
+
+// if (isNaN(n)) {
+//   console.log(1);
+// } else {
+//   console.log(factorial(n));
+// }
+
+
+  
