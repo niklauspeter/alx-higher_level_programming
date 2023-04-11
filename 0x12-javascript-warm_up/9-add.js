4d86de8b18229e087a9f1bd792c4be478deadc5b
klaus <oriokiklaus@gmail.com>
2023-04-12 01:53:27 +0300

    create function and use it to print sum of two arguments

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+function add(a, b)
+{
+    return a + b;
+}
+const a = Number(process.argv[2]);
+const b = Number(process.argv[3]);
+
+if (a || b) {
+    console.log(add(a, b));
+}
+else {
+    console.log("NaN")
+}
