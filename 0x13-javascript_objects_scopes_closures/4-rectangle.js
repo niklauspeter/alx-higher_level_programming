7f4bd6b264f1c0a664fa0a2bfbdc8665a7975508
klaus <oriokiklaus@gmail.com>
2023-04-13 03:14:45 +0300

    correct to multiply each side by double itself

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -19,7 +19,7 @@ module.exports = class Rectangle {
   }
 
   double () {
-    this.width = this.height * 2;
-    this.height = this.width * 2;
+    this.width = this.width * 2;
+    this.height = this.height * 2;
   }
 };
