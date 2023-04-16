2e56e6d0ac9c04d17440441be62cba511792d472
klaus <oriokiklaus@gmail.com>
2023-04-17 00:33:54 +0300

    add condition not to execute when imported

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,10 +6,11 @@ args; username, password and databasename
 import sys
 import MySQLdb
 
-db = MySQLdb.connect("user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]")
-cur = db.cursor()
-cur.execute("SELECT * FROM `states` ORDER BY id")
-rows = cur.fetchall()
-for state in rows:
-    if state[1][0] == "N":
-        print (state)
+if __name__ == "__main__":
+    db = MySQLdb.connect("user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM `states` ORDER BY id")
+    rows = cur.fetchall()
+    for state in rows:
+        if state[1][0] == "N":
+            print (state)
