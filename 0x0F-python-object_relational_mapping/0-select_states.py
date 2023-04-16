40310f23a42a3eedc3af8d2300e9650f60ea8264
klaus <oriokiklaus@gmail.com>
2023-04-17 00:05:17 +0300

    add line at end

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """
-Lists all states from the database hbtn_0e_0_usa.
-Usage: ./0-select_states.py <mysql username> \
-                            <mysql password> \
-                             <database name>
+Script lists all states from the database hbtn_0e_0_usa.
+parameters include username, password and database name.
 """
 import sys
 import MySQLdb
