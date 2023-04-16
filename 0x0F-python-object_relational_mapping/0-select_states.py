7c5340637645a0dc36512d4225f0f7236b080a6e
klaus <oriokiklaus@gmail.com>
2023-04-17 00:18:33 +0300

    assign c.fetchall method to row variable for easy readability

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,6 +8,7 @@ import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states` BY id ASC")
-    [print(state) for state in c.fetchall()]
+    cur = db.cursor()
+    cur.execute("SELECT * FROM `states` BY id ASC")
+    rows = cur.fetchall()
+    [print(state) for state in rows]
