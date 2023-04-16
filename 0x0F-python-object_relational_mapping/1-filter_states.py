afc0e2eebed4aaadef3846105d6f65f032008f66
klaus <oriokiklaus@gmail.com>
2023-04-17 00:29:37 +0300

    prints states starting with capital N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+script lists all states with name starting with capital N
+args; username, password and databasename
+"""
+import sys
+import MySQLdb
+
+db = MySQLdb.connect("user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]")
+cur = db.cursor()
+cur.execute("SELECT * FROM `states` ORDER BY id")
+rows = cur.fetchall()
+for state in rows:
+    if state[1][0] == "N":
+        print (state)
