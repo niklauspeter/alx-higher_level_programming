6cd8916504639a63561048dcb0235a5a458ded9f
klaus <oriokiklaus@gmail.com>
2023-04-17 00:43:07 +0300

    make executable

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
-Script lists all states from the database hbtn_0e_0_usa.
-parameters include username, password and database name.
+Lists all states from the database hbtn_0e_0_usa.
+Usage: ./0-select_states.py <mysql username> \
+                            <mysql password> \
+                             <database name>
 """
 import sys
 import MySQLdb
@@ -9,6 +11,6 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM `states` BY id ASC")
+    cur.execute("SELECT * FROM `states`")
     rows = cur.fetchall()
     [print(state) for state in rows]
