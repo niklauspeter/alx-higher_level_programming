9b8277a80b4a200d7441d2213a0a9363b3110589
klaus <oriokiklaus@gmail.com>
2023-04-28 02:18:30 +0300

    change comment to one line

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Script sends request to particular URL and
-# Get the byte size of the HTTP response header.
+# Script sends request to particular URL and get the byte size of the HTTP response header.
 curl -s "$1" | wc -c
