601d9b41c7f87196cc0223ee6a99d18206abd7c4
klaus <oriokiklaus@gmail.com>
2023-04-28 17:33:41 +0300

    remove trailing whitespace

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,8 +3,8 @@
 Python script that takes in a URL,
 sends a request to the URL
 and displays the value of the X-Request-Id variable
-found in the header of the response."""
-
+found in the header of the response.
+"""
 import urllib.request
 import sys
 
