92b72eda949ef51989aaf0c4b3f4c7684a769d74
klaus <oriokiklaus@gmail.com>
2023-04-28 17:32:18 +0300

    add urllib package nametag

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,12 +5,12 @@ sends a request to the URL
 and displays the value of the X-Request-Id variable
 found in the header of the response."""
 
-from urllib import request
+import urllib.request
 import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    req = request.Request(url)
-    with request.urlopen(req) as response:
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
         print(dict(response.headers).get('X-request-Id'))
