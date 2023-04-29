3027237c1853ad8b6abf21575b764e1714e6be3a
klaus <oriokiklaus@gmail.com>
2023-04-29 03:25:35 +0300

    make it so it doesn't execut(name == main)

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """
 sript takes in url, sneds a request to the url and
-displays value of variable x-request-d in response header"""
+displays value of variable x-request-d in response header
+"""
 
 import sys
 import requests
 
-if "__main__" == "__name__":
+if "__name__" == "__main__":
     url = sys.argv[1]
 
     req = requests.get(url)
