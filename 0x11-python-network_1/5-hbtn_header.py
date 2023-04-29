171b68ea99a9049a3aa2d920f959b8c27f3171e5
klaus <oriokiklaus@gmail.com>
2023-04-29 03:18:26 +0300

    sript takes in url, sneds a request to the url and displays value of variable x-request-d in response header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+sript takes in url, sneds a request to the url and
+displays value of variable x-request-d in response header"""
+
+import sys
+import requests
+
+if "__main__" == "__name__":
+    url = "sys.argv[1]"
+
+    req = requests.get(url)
+    print(req.headers.get("X-request"))
