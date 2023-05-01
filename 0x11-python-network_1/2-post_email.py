aa8087cc0d88955bc1426ee15c1013ddcdb10248
klaus <oriokiklaus@gmail.com>
2023-05-01 20:57:59 +0300

    add spacing

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """
 script sends post request to given URL with email
-and displays the body of the response"""
-
+and displays the body of the response
+"""
 import sys
-import urllib.request
 import urllib.parse
+import urllib.request
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
+    val = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(val).encode("ascii")
 
     request = urllib.request.Request(url, data)
     with urllib.request.urlopen(request) as response:
-        print(response.read().decode("urf-8"))
+        print(response.read().decode("utf-8"))
