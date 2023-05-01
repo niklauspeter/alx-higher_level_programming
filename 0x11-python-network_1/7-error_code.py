f32c6a8cd6fbe9d8cda829da34e13ae6f4e44f70
klaus <oriokiklaus@gmail.com>
2023-05-01 21:13:13 +0300

    correct trailing white space

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" script takes in a URL, 
-sends a request to the URL 
+""" script takes in a URL,
+sends a request to the URL
 and displays the body of the response.
 """
-
 import sys
 import requests
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
